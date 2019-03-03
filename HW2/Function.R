507ee49f1126ed3876f569119f6ada70988dabc4
znzhao <znzhao@utexas.edu>
2019-03-03 14:52:06 -0600

    finished

--- HW2/Function.R
+++ HW2/Function.R
@@ -3,6 +3,7 @@ library(mosaic)
 library(class)
 library(FNN)
 
+
 rmse = function(y, yhat) {
   sqrt( mean( (y - yhat)^2 ) )
 }
@@ -34,7 +35,30 @@ lm_avgrmse = function(fo, data, Ntimes = 50){
   colMeans(rmse_vals)
 }
 
-
+lm_C_error = function(fo, data, y, threshold = 0.5, Ntimes = 50){
+  n = nrow(data)
+  # performance check
+  rmse_vals = do(Ntimes)*{
+    
+    # re-split into train and test cases
+    n_train = round(0.8*n)  # round to nearest integer
+    n_test = n - n_train
+    train_cases = sample.int(n, n_train, replace=FALSE)
+    test_cases = setdiff(1:n, train_cases)
+    DF_train = data[train_cases,]
+    DF_test = data[test_cases,]
+    y_test = y[test_cases]
+    
+    # fit to this training set
+    lm_result = lm(fo, data=DF_train)
+    
+    # predict on this testing set
+    phat_test = predict(lm_result, DF_test)
+    yhat_test = ifelse((phat_test>threshold), yes = 1 ,no = 0)
+    sum(yhat_test != y_test)/n_test 
+    }
+  colMeans(rmse_vals)
+}
 
 glm_C_error = function(fo, data, Family = binomial, threshold = 0.5, Ntimes = 50){
   n = nrow(data)
@@ -91,7 +115,7 @@ KNN_avgrmse = function(data_X, data_y, K = 2, Ntimes = 50){
 
 
 KNN_C_error = function(data_X, data_y, K = 2, Ntimes = 50){
-  n = length(data_y)
+  n = nrow(data_X)
   C_error = do(Ntimes)*{
     # re-split into train and test cases
     n_train = round(0.8*n)  # round to nearest integer
@@ -100,8 +124,8 @@ KNN_C_error = function(data_X, data_y, K = 2, Ntimes = 50){
     train_ind = sample.int(n, n_train)
     X_train = X[train_ind,]
     X_test = X[-train_ind,]
-    y_train = y[train_ind]
-    y_test = y[-train_ind]
+    y_train = y[train_ind,]
+    y_test = y[-train_ind,]
     
     # scaling
     scale_factors = apply(X_train, 2, sd)
@@ -118,3 +142,32 @@ KNN_C_error = function(data_X, data_y, K = 2, Ntimes = 50){
   colMeans(C_error)
 }
 
+
+KNN_C_error_linear = function(data_X, data_y, obj_y, K = 2, threshold, Ntimes = 50){
+  n = nrow(data_X)
+  rmse_vals_K = do(Ntimes)*{
+    # re-split into train and test cases
+    n_train = round(0.8*n)  # round to nearest integer
+    n_test = n - n_train
+    train_cases = sample.int(n, n_train, replace=FALSE)
+    test_cases = setdiff(1:n, train_cases)
+    X_train = data_X[train_cases,]
+    X_test = data_X[test_cases,]
+    y_train = data_y[train_cases,]
+    y_test = obj_y[test_cases,]
+    
+    # scaling
+    scale_factors = apply(X_train, 2, sd)
+    X_train_sc = scale(X_train, scale=scale_factors)
+    # scale the test set features using the same scale factors
+    X_test_sc = scale(X_test, scale=scale_factors)
+    
+    
+    # Fit the KNN model (notice the odd values of K)
+    knn_K = FNN::knn.reg(train=X_train_sc, test= X_test_sc, y = y_train, k=K)
+    phat_test = knn_K$pred
+    yhat_test = ifelse((phat_test>threshold), yes = 1 ,no = 0)
+    sum(yhat_test != y_test)/n_test 
+  }
+  colMeans(rmse_vals_K)
+}
--- HW2/HW2.3.R
+++ HW2/HW2.3.R
@@ -3,6 +3,7 @@ library(mosaic)
 library(class)
 library(FNN)
 
+
 urlfile<-'https://raw.githubusercontent.com/znzhao/ECO395M-HW-zz5339/master/HW2/online_news.csv'
 OnlineNews<-read.csv(url(urlfile))
 
@@ -12,7 +13,11 @@ summary(OnlineNews)
   p1 <- ggplot(data = OnlineNews) + 
     geom_point(mapping = aes(y = shares, x =num_imgs),alpha = 0.01) + ylim(0, 2000)
   p1
-
+  
+  p2 <- ggplot(data = OnlineNews) + 
+    geom_density(mapping = aes(x = shares),alpha = 0.01)+xlim(0,5000)
+  p2
+  
 ### Split into training and testing sets
 n = nrow(OnlineNews)
 n_train = round(0.8*n)  # round to nearest integer
@@ -110,4 +115,65 @@ sum(OnlineNews_train$viral)/count(OnlineNews_train)
 # }
 # rmse(OnlineNews_test$shares, yhat_test1)
 
+######################################################################################
+# Zhenning
+######################################################################################
+lm_C_error(shares ~ num_self_hrefs +
+             (n_tokens_title + n_tokens_content + num_hrefs + 
+                num_imgs + num_videos + 
+                average_token_length + num_keywords + weekday_is_monday+ weekday_is_tuesday+ weekday_is_wednesday+weekday_is_thursday+weekday_is_friday+weekday_is_saturday + self_reference_avg_sharess)*
+             (data_channel_is_lifestyle +
+                data_channel_is_entertainment + data_channel_is_bus + 
+                + data_channel_is_socmed + data_channel_is_tech +
+                data_channel_is_world) ,
+           data=OnlineNews, y = OnlineNews$viral, threshold = 1400, Ntimes = 50)
 
+summary(lm(shares ~ num_self_hrefs +  num_hrefs+weekday_is_monday+ weekday_is_tuesday+ weekday_is_wednesday+weekday_is_thursday+weekday_is_friday+weekday_is_saturday + 
+             (n_tokens_title + n_tokens_content + 
+                num_imgs + num_videos + 
+                average_token_length + num_keywords + self_reference_avg_sharess)*
+             (data_channel_is_lifestyle +
+                data_channel_is_entertainment + data_channel_is_bus + 
+                + data_channel_is_socmed + data_channel_is_tech +
+                data_channel_is_world) ,
+           data=OnlineNews))
+
+glm_C_error(viral ~ num_self_hrefs +  num_hrefs+weekday_is_monday+ weekday_is_tuesday+ weekday_is_wednesday+weekday_is_thursday+weekday_is_friday+weekday_is_saturday + 
+              (n_tokens_title + n_tokens_content + 
+                 num_imgs + num_videos + 
+                 average_token_length + num_keywords + self_reference_avg_sharess)*
+              (data_channel_is_lifestyle +
+                 data_channel_is_entertainment + data_channel_is_bus + 
+                 + data_channel_is_socmed + data_channel_is_tech +
+                 data_channel_is_world), threshold = 0.5,
+             data=OnlineNews,Ntimes = 10)
+
+X = subset(OnlineNews,select = c(num_self_hrefs,n_tokens_title,n_tokens_content,num_hrefs, num_imgs,num_videos,average_token_length,num_keywords, is_weekend,data_channel_is_lifestyle,data_channel_is_entertainment,data_channel_is_bus,data_channel_is_socmed, data_channel_is_tech,data_channel_is_world, self_reference_avg_sharess))
+x = subset(OnlineNews,select = c(num_self_hrefs,n_tokens_title,n_tokens_content,num_hrefs, num_imgs,num_videos,average_token_length,num_keywords, is_weekend,data_channel_is_lifestyle,data_channel_is_entertainment,data_channel_is_bus,data_channel_is_socmed, data_channel_is_tech,data_channel_is_world, self_reference_avg_sharess, weekday_is_monday, weekday_is_tuesday, weekday_is_wednesday, weekday_is_thursday, weekday_is_friday, weekday_is_saturday, weekday_is_sunday,global_rate_positive_words, global_rate_negative_words, avg_positive_polarity, min_positive_polarity, max_positive_polarity, avg_negative_polarity, min_negative_polarity, max_negative_polarity, title_subjectivity, title_sentiment_polarity, abs_title_sentiment_polarity))
+
+Y = subset(OnlineNews,select = c(viral))
+
+KNN_result <- data.frame(K=c(), rsme=c())
+k_grid = seq(53, 250, by=30)
+for(v in k_grid){
+  avgrmse = KNN_C_error(x,Y, K = v,Ntimes = 5)
+KNN_result <- rbind(KNN_result,c(v,avgrmse))
+}
+colnames(KNN_result) <- c("K","AVG_RMSE")
+ggplot(data = KNN_result, aes(x = K, y = AVG_RMSE)) + 
+  geom_point(shape = "O") +
+  geom_line(col = "red")
+
+
+obj_Y = subset(OnlineNews,select = c(viral))
+
+KNN_result <- data.frame(K=c(), rsme=c())
+k_grid = seq(53, 250, by=30)
+for(v in k_grid){
+  avgrmse = KNN_C_error_linear(data_X = x,data_y = Y,obj_y = obj_Y, K = v,threshold = 1400,Ntimes = 5)
+  KNN_result <- rbind(KNN_result,c(v,avgrmse))
+}
+colnames(KNN_result) <- c("K","AVG_RMSE")
+ggplot(data = KNN_result, aes(x = K, y = AVG_RMSE)) + 
+  geom_point(shape = "O") +
+  geom_line(col = "red")
