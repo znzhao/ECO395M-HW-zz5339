5739bc1a2bd747308f83766c93adb777dc6100be
znzhao <znzhao@utexas.edu>
2019-03-14 16:47:06 -0500

    test

--- HW2/HW2.1.R
+++ HW2/HW2.1.R
@@ -4,6 +4,38 @@ library(class)
 library(FNN)
 data(SaratogaHouses)
 
+rmse = function(y, yhat) {
+  sqrt( mean( (y - yhat)^2 ) )
+}
+
+KNN_avgrmse = function(data_X, data_y, K = 2, Ntimes = 50){
+  n = nrow(data_X)
+  rmse_vals_K = do(Ntimes)*{
+    # re-split into train and test cases
+    n_train = round(0.8*n)  # round to nearest integer
+    n_test = n - n_train
+    train_cases = sample.int(n, n_train, replace=FALSE)
+    test_cases = setdiff(1:n, train_cases)
+    X_train = data_X[train_cases,]
+    X_test = data_X[test_cases,]
+    y_train = data_y[train_cases,]
+    y_test = data_y[test_cases,]
+    
+    # scaling
+    scale_factors = apply(X_train, 2, sd)
+    X_train_sc = scale(X_train, scale=scale_factors)
+    # scale the test set features using the same scale factors
+    X_test_sc = scale(X_test, scale=scale_factors)
+    
+    
+    # Fit the KNN model (notice the odd values of K)
+    knn_K = FNN::knn.reg(train=X_train_sc, test= X_test_sc, y = y_train, k=K)
+    ypred_knn = knn_K$pred
+    rmse(y_test, ypred_knn)
+  }
+  colMeans(rmse_vals_K)
+}
+
 summary(SaratogaHouses)
 
 # 11 main effects
@@ -33,33 +65,55 @@ SaratogaHouses = cbind(SaratogaHouses,temp)
 temp = model.matrix( ~ centralAir-1,SaratogaHouses)
 SaratogaHouses = cbind(SaratogaHouses,temp)
 
-c(
-# model 1
-lm_avgrmse(price ~ lotSize + age + livingArea + pctCollege + bedrooms + fireplaces + bathrooms + rooms + heating + fuel + centralAir, SaratogaHouses, Ntimes = 100),
-# model 2
-lm_avgrmse(price ~ lotSize + age + livingArea + pctCollege*fireplaces + bedrooms  + bathrooms + rooms + heating + centralAir, SaratogaHouses, Ntimes = 100),
-# model 3
-lm_avgrmse(price ~ landValue + livingArea + pctCollege + bedrooms + fireplaces + bathrooms + rooms + heating + fuel + centralAir, SaratogaHouses, Ntimes = 100),
-# model 4
-lm_avgrmse(price ~ landValue + livingArea + bedrooms + bathrooms + rooms + heating + fuel + centralAir, SaratogaHouses, Ntimes = 100),
-# model 5, which is the best
-lm_avgrmse(price ~ landValue + lotSize*bedrooms + livingArea*fuel + pctCollege*(fireplaces+age) + bathrooms + rooms + heating + centralAir, SaratogaHouses, Ntimes = 100),
-# model 6
-lm_avgrmse(price ~ landValue + lotSize*bedrooms + livingArea*fuel + pctCollege*(fireplaces+age) + centralAir, SaratogaHouses, Ntimes = 100)
-)
+data = SaratogaHouses
+
+n = nrow(SaratogaHouses)
+Ntimes = 100
+# performance check
+rmse_vals = do(Ntimes)*{
+  # re-split into train and test cases
+  n_train = round(0.8*n)  # round to nearest integer
+  n_test = n - n_train
+  train_cases = sample.int(n, n_train, replace=FALSE)
+  test_cases = setdiff(1:n, train_cases)
+  DF_train = data[train_cases,]
+  DF_test = data[test_cases,]
+  
+  # fit to this training set
+  lm_result1 = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=DF_train)
+  lm_result2 = lm(price ~ lotSize + age + livingArea + pctCollege*fireplaces + bedrooms  + bathrooms + rooms + heating + centralAir, data=DF_train)
+  lm_result3 = lm(price ~ landValue + livingArea + pctCollege + bedrooms + fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=DF_train)
+  lm_result4 = lm(price ~ landValue + livingArea + bedrooms + bathrooms + rooms + heating + fuel + centralAir, data=DF_train)
+  # the best regression
+  lm_result5 = lm(price ~ landValue + lotSize*(bedrooms + bathrooms) + livingArea*(fuel+ heating + centralAir) + pctCollege*(fireplaces+age) + rooms, data=DF_train)
+  lm_result6 = lm(price ~ landValue + lotSize*bedrooms + livingArea*fuel + pctCollege*(fireplaces+age) + bathrooms + rooms + heating + centralAir, data=DF_train)
+  lm_result7 = lm(price ~ landValue + lotSize*bedrooms + livingArea*fuel + pctCollege*(fireplaces+age) + centralAir, data=DF_train)
+  
+  # predict on this testing set
+  yhat_test1 = predict(lm_result1, DF_test)
+  yhat_test2 = predict(lm_result2, DF_test)
+  yhat_test3 = predict(lm_result3, DF_test)
+  yhat_test4 = predict(lm_result4, DF_test)
+  yhat_test5 = predict(lm_result5, DF_test)
+  yhat_test6 = predict(lm_result6, DF_test)
+  yhat_test7 = predict(lm_result7, DF_test)
+  
+  c(rmse(unlist(DF_test$price), yhat_test1), rmse(unlist(DF_test$price), yhat_test2), rmse(unlist(DF_test$price), yhat_test3), rmse(unlist(DF_test$price), yhat_test4), rmse(unlist(DF_test$price), yhat_test5), rmse(unlist(DF_test$price), yhat_test6), rmse(unlist(DF_test$price), yhat_test7))
+}
+
+colMeans(rmse_vals)
+t = as.data.frame(colMeans(rmse_vals))
+rownames(t) <- c("AVG RMSE")
+summary(lm(price ~ landValue + lotSize*(bedrooms + bathrooms) + livingArea*(fuel+ heating + centralAir) + pctCollege*(fireplaces+age) + rooms, data=data))
 
 
-
-
-X = subset(SaratogaHouses, select=c(-price,-waterfront,-sewer,-newConstruction,-heating, -fuel, -centralAir, -centralAirNo, -fueloil, -heatinghotair))
-x = subset(X, select=c(-heatingelectric, -heatingwatersteam, -fuelgas,-fuelelectric, -centralAirYes))
-
+X = subset(SaratogaHouses, select=c(-price,-waterfront,-sewer,-newConstruction,-heating, -fuel, -centralAir, -centralAirNo, -fuelgas, -heatinghotair))
 y = subset(SaratogaHouses, select=c(price))
 
 KNN_result <- data.frame(K=c(), rsme=c())
 k_grid = seq(3, 30, by=1)
 for(v in k_grid){
-avgrmse = KNN_avgrmse(data_X = X, data_y = y, K = v, Ntimes = 50)
+avgrmse = KNN_avgrmse(data_X = X, data_y = y, K = v, Ntimes = 200)
 KNN_result <- rbind(KNN_result,c(v,avgrmse))
 }
 
--- HW2/report/Exercise_2_report.md
+++ HW2/report/Exercise_2_report.md
@@ -2,3 +2,85 @@ Exercise 2
 ==========
 
 By Chong Wang, Tianping Wu, Zhenning Zhao
+
+|         | AVG RMSE |
+|---------|:--------:|
+| model 1 | 66068.44 |
+| model 2 | 65825.60 |
+| model 3 | 59765.10 |
+| model 4 | 59677.72 |
+| model 5 | 59126.45 |
+| model 6 | 59432.07 |
+| model 7 | 60182.50 |
+
+``` r
+summary(lm(price ~ landValue + lotSize*(bedrooms + bathrooms) + livingArea*(fuel+ heating + centralAir) + pctCollege*(fireplaces+age) + rooms, data=data))
+```
+
+    ## 
+    ## Call:
+    ## lm(formula = price ~ landValue + lotSize * (bedrooms + bathrooms) + 
+    ##     livingArea * (fuel + heating + centralAir) + pctCollege * 
+    ##     (fireplaces + age) + rooms, data = data)
+    ## 
+    ## Residuals:
+    ##     Min      1Q  Median      3Q     Max 
+    ## -231721  -34928   -5458   27010  453713 
+    ## 
+    ## Coefficients:
+    ##                                     Estimate Std. Error t value Pr(>|t|)
+    ## (Intercept)                       -8.643e+03  1.770e+04  -0.488 0.625456
+    ## landValue                          8.332e-01  4.816e-02  17.302  < 2e-16
+    ## lotSize                            8.886e+03  8.083e+03   1.099 0.271757
+    ## bedrooms                          -9.011e+03  3.052e+03  -2.953 0.003192
+    ## bathrooms                          2.423e+04  3.873e+03   6.257 4.95e-10
+    ## livingArea                         9.013e+01  5.690e+00  15.841  < 2e-16
+    ## fuelelectric                      -4.945e+04  4.874e+04  -1.015 0.310437
+    ## fueloil                            3.909e+04  1.311e+04   2.981 0.002913
+    ## heatinghot water/steam             1.278e+04  1.277e+04   1.001 0.317140
+    ## heatingelectric                    4.674e+04  4.936e+04   0.947 0.343806
+    ## centralAirNo                       3.461e+04  1.046e+04   3.308 0.000960
+    ## pctCollege                         1.983e+01  2.603e+02   0.076 0.939288
+    ## fireplaces                         4.139e+04  1.479e+04   2.798 0.005203
+    ## age                               -5.980e+02  2.613e+02  -2.288 0.022234
+    ## rooms                              2.540e+03  9.777e+02   2.598 0.009466
+    ## lotSize:bedrooms                   1.639e+03  2.743e+03   0.598 0.550087
+    ## lotSize:bathrooms                 -3.076e+03  3.420e+03  -0.899 0.368534
+    ## livingArea:fuelelectric            2.421e+01  2.804e+01   0.863 0.388031
+    ## livingArea:fueloil                -2.464e+01  7.455e+00  -3.306 0.000967
+    ## livingArea:heatinghot water/steam -1.069e+01  6.774e+00  -1.577 0.114871
+    ## livingArea:heatingelectric        -2.825e+01  2.860e+01  -0.988 0.323433
+    ## livingArea:centralAirNo           -2.527e+01  5.479e+00  -4.612 4.28e-06
+    ## pctCollege:fireplaces             -7.023e+02  2.601e+02  -2.700 0.007011
+    ## pctCollege:age                     1.043e+01  4.880e+00   2.137 0.032735
+    ##                                      
+    ## (Intercept)                          
+    ## landValue                         ***
+    ## lotSize                              
+    ## bedrooms                          ** 
+    ## bathrooms                         ***
+    ## livingArea                        ***
+    ## fuelelectric                         
+    ## fueloil                           ** 
+    ## heatinghot water/steam               
+    ## heatingelectric                      
+    ## centralAirNo                      ***
+    ## pctCollege                           
+    ## fireplaces                        ** 
+    ## age                               *  
+    ## rooms                             ** 
+    ## lotSize:bedrooms                     
+    ## lotSize:bathrooms                    
+    ## livingArea:fuelelectric              
+    ## livingArea:fueloil                ***
+    ## livingArea:heatinghot water/steam    
+    ## livingArea:heatingelectric           
+    ## livingArea:centralAirNo           ***
+    ## pctCollege:fireplaces             ** 
+    ## pctCollege:age                    *  
+    ## ---
+    ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+    ## 
+    ## Residual standard error: 59090 on 1704 degrees of freedom
+    ## Multiple R-squared:  0.6445, Adjusted R-squared:  0.6397 
+    ## F-statistic: 134.3 on 23 and 1704 DF,  p-value: < 2.2e-16
